db48784aa7b6835f29154d43f933998e2c5d25f8
Mloea2412 <103989630+Mloea2412@users.noreply.github.com>
2025-02-05 14:54:03 +0100

    Add files via upload

---
+++ Modul01/Modul1/Modul1/Program.cs
@@ -0,0 +1,14 @@
+﻿/*// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Hej Hej");
+string velkomst = "Velkommen til app udvikling";
+Console.WriteLine(velkomst);
+Console.WriteLine("Skriv dit navn");
+string navn = Console.ReadLine();
+Console.WriteLine("Hej " + navn);
+// min kommentar....
+Console.Write("5* 12 =");
+Console.WriteLine(5 * 12);
+*/
---
+++ Modul01/Modul1/Opgave1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*******
+
+Lav et nyt projekt i Visual Studio som konsol applikation som i giver en givende navn (skal bruges både til denne og næste opgave). 
+
+(Hint: Du kan få lidt hjælp fra side 69 i bogen)
+
+1. Udskriv resultatet af et plus-stykke
+2. Udskriv resultatet af et minus-stykke
+3. Udskriv resultatet af et gange-stykke
+4. Udskriv resultatet af et divisions-stykke der går op 
+5. Udskriv resultatet af et divisions-stykke der ikke går op 
+6. Udskriv resultatet af dette divisionsstykke "rest" (kaldes modulo) (et eksempel på brug af modulo er at 5 % 2 giver 1. Hvorfor?)
+7. Udskriv resultatet af følgende givne matematikopgaver:
+-1 + 4 * 6
+(35 + 5) % 7
+14 + -4 * 6 / 11
+2 + 15 / 6 * 1 - 7 % 2 */
+
+
